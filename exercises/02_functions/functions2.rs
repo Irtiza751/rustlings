0b2974e8b5873823c0577c9cd16d89998db81a22
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-06-02 12:05:42 +0500

    Completed intro, variables, functions

--- exercises/02_functions/functions2.rs
+++ exercises/02_functions/functions2.rs
@@ -3,14 +3,14 @@
 // Execute `rustlings hint functions2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
     call_me(3);
 }
 
-fn call_me(num:) {
-    for i in 0..num {
-        println!("Ring! Call number {}", i + 1);
+fn call_me(num: i32) {
+    for i in 0..num + 2 {
+        println!("Ring! Call number {}", i + 1 * 2);
     }
 }
