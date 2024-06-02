e730f08a6e10c0c898d60fb4bb74a6bf936c2f1e
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-06-02 12:40:15 +0500

    complete the if exercise

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -2,11 +2,17 @@
 //
 // Execute `rustlings hint if1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 pub fn bigger(a: i32, b: i32) -> i32 {
     // Complete this function to return the bigger number!
     // If both numbers are equal, any of them can be returned.
+    if a == b {
+        return a;
+    } else if a > b {
+        return a;
+    }
+    return b;
     // Do not use:
     // - another function call
     // - additional variables
